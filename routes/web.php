4244316ea65f4db3f6cdc8102af3daee76d946c7
unknown <alexanderpaullunas@gmail.com>
2024-11-15 22:54:46 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'index']);
+Route::resource('products', ProductController::class);
+
+
+
+
+
